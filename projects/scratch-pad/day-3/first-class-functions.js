06c4b60d52056f1ef9b02ddc6e94c0c2828486d4
ChrisLeboeuf <christopherleboeuf1@gmail.com>
2019-10-17 19:47:18 +0000

    Completed scratch-pad

---
+++ projects/scratch-pad/day-3/first-class-functions.js
@@ -0,0 +1,132 @@
+// #!/usr/bin/env node
+
+'use strict';
+
+/**
+ * IN CLASS EXERCISE: FIRST CLASS FUNCTIONS
+ */
+
+/** 
+ * Given an input base to test against, which could be a String or Number, 
+ * return a Function that tests whether a given value is greater than the 
+ * base.
+ */
+function createGreaterThanFilter(base) {
+    // YOUR CODE BELOW HERE //
+    
+    //return function with a given number as an argument
+    return function(givenNumber){
+       //test if the given is greater than the base
+       return givenNumber > base;
+       
+   };
+   
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an input base to test against, which could be a String or Number, 
+ * return a Function that tests whether a given value is less than the 
+ * base.
+ */
+function createLessThanFilter(base) {
+    // YOUR CODE BELOW HERE //
+    
+    //return function with a given number as an argument
+    return function(givenNumber){
+        //test if the given is less than the base
+       return givenNumber < base;
+       
+   };
+    
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given a startsWith character, which will be a single character, return a 
+ * Function that tests whether a given String starts with the startsWith 
+ * character.
+ */
+function createStartsWithFilter(startsWith) {
+    // YOUR CODE BELOW HERE //
+    //return a function with a given string as an argument
+    return function(givenString){
+        //return
+        return givenString.toLowerCase().startsWith(startsWith.toLowerCase());
+    };
+    
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given a endsWith character, which will be a single character, return a 
+ * Function that tests whether a given String ends with the endsWith 
+ * character.
+ */
+function createEndsWithFilter(endsWith) {
+    // YOUR CODE BELOW HERE //
+       return function(givenString){
+        //return
+        return givenString.toLowerCase().endsWith(endsWith.toLowerCase());
+    };
+    
+    
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an Array of Strings and a Function designed to modify a String, 
+ * return an Array of the Strings, modified.
+ * 
+ * TIP: You need to loop over the Strings, right? We need to pass each String to 
+ * the modify Function, but we need to collect the results into some collection.
+ */
+function modifyStrings(strings, modify) {
+    // YOUR CODE BELOW HERE //
+    var arr = [];
+    for(var i = 0; i < strings.length; i++){
+       arr.push(modify(strings[i]));
+    }
+    return arr
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an Array of Strings and a Function designed to test the String in some 
+ * way and return a Boolean on whether it passed, return true if ALL Strings pass the test.
+ * 
+ * Imagine you had a list of names, and you wanted to test they all 
+ * begin with "C", or they are all exclaimations that end with "!".
+ * 
+ * TIP: You need to loop over the Strings, right? And pass them to the test?
+ */
+function allStringsPass(strings, test) {
+    // YOUR CODE BELOW HERE //
+    var j = 0;
+    //for every element in the array
+    for(var i = 0; i < strings.length; i++){
+        //test if the element passes a given test
+        if(test(strings[i])){
+            j += 0;
+        } else {
+            j++;
+        }
+    }
+    return j === 0;
+    // YOUR CODE ABOVE HERE //
+}
+
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
+if((typeof process !== 'undefined') &&
+    (typeof process.versions.node !== 'undefined')) {
+    // here, export any references you need for tests //
+    module.exports.createGreaterThanFilter = createGreaterThanFilter;
+    module.exports.createLessThanFilter = createLessThanFilter;
+    module.exports.createStartsWithFilter = createStartsWithFilter;
+    module.exports.createEndsWithFilter = createEndsWithFilter;
+    module.exports.modifyStrings = modifyStrings;
+    module.exports.allStringsPass = allStringsPass;   
+}
---
+++ projects/scratch-pad/day-4/contact-list.js
@@ -0,0 +1,108 @@
+// #!/usr/bin/env node
+
+'use strict';
+
+/**
+ * 4: Contact List
+ * 
+ *  a. Create a factory Function called makeContact(id, nameFirst, nameLast) 
+ *     that returns a contact object.
+ *     
+ *          ex: makeContact(1, 'Max', 'Gaudin'); // => {id: 1, nameFirst: 'Max', nameLast: 'Gaudin'}
+ *     
+ *  b. Create a factory Function called makeContactList that returns an Object 
+ *     that manages contacts. The contact-list object should have the following API:
+ *       
+ *      1. length(): returns the number of contacts within the list.
+ *      2. addContact(contact): takes a contact object to be added to the 
+ *         contact-list.
+ *      3. findContact(fullName): takes a full-name String, like 'Max Gaudin', and 
+ *         returns the contact object if found in the contacts-list, or, 
+ *         undefined if the fullName does not match any contacts in the list.
+ *      4. removeContact(contact): takes a contact object to be removed from 
+ *         the contact-list.
+ * 
+ * BONUS : add a printAllContactNames() Function to your makeContactList() factory, so that the 
+ *         contact-list returned has an all() API. The printAllContactNames() Function should 
+ *         return a String formated with all the full-names of the separated 
+ *         with a line-break, like so:
+ *          
+ *         myContacts.printAllContactNames(); // => Max Gaudin
+ *                                                  John Fraboni
+ *                                                  Your Mom
+ *          
+ *          WARNING: To pass the bonus test, the LAST full name should have NO
+ *          new-line character added after it!
+ */
+
+// YOUR CODE GOES BELOW HERE //
+function makeContact(id, nameFirst, nameLast) {
+return {'id': id, 'nameFirst': nameFirst, 'nameLast': nameLast}
+} 
+
+
+function makeContactList() {
+    /*
+     * You need something here to hold contacts. See length api for a hint:
+     */
+    var contacts = [];
+    
+    return {
+        // we implemented the length api for you //
+        length: function() {
+            return contacts.length;
+        },
+        //create addContact function with 1 perameter. contact.
+        addContact: function(contact){
+            contacts.push(contact);
+        },
+        
+        findContact: function(fullName){
+            var contact;
+            //search through contacts using a for loop
+            for(let i = 0; i < contacts.length; i++){
+                //if contacts first name and last name together matches the fullName
+                if( (contacts[i].nameFirst + ' ' + contacts[i].nameLast) === fullName){
+                    //set matching contact to the empty contact var
+                    contact = contacts[i];
+                }
+            }
+            //return matching contact
+            return contact;
+        },
+        
+        removeContact: function(contact){
+            //filter out contacts
+            contacts = contacts.filter(item => item !== contact);
+        },
+        
+        printAllContactNames: function(){
+            var contactList = '';
+            //search through the contact list 
+            for(let i = 0; i < contacts.length; i++){
+                //concatenate each contacts full name to contact list with a new line character
+                contactList += (contacts[i].nameFirst + ' ' + contacts[i].nameLast) + '\n';
+            }
+            //remove last character because there would be an extra new line character
+            contactList = contactList.substring(0, contactList.length - 1);
+            //return the list of all contact names
+            return contactList;
+        },
+    }
+}
+
+
+
+
+// YOUR CODE GOES ABOVE HERE //
+
+
+
+
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
+if((typeof process !== 'undefined') &&
+(typeof process.versions.node !== 'undefined')) {
+    // here, export any references you need for tests //
+    module.exports.makeContact = makeContact;
+    module.exports.makeContactList = makeContactList;
+}
